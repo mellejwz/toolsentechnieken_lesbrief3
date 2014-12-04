ef134433dd40569c01372b4b592b10b979610064
Melle Zwerver <mellezwerver@outlook.com>
2014-12-04 15:45:24 +0100

    comments toegevoegd

--- the real stuff/1.2.2.rb
+++ the real stuff/1.2.2.rb
@@ -27,7 +27,7 @@ def add_contact()
 		'',
 		'addressbook'
 		)
-
+	#ask for contact details and save those in variables
 	puts 'Please input the name of the contact'
 	name = gets.chomp
 	puts 'Please input the last name of the contact'
@@ -36,7 +36,7 @@ def add_contact()
 	email = gets.chomp
 	puts 'Please input the phone number of the contact'
 	phone = gets.chomp
-
+	#insert contact details into a new record in the table
 	connection.query 'INSERT INTO contacts (Naam, Achternaam, Email, Nummer) VALUES (
 			"'+name.to_s+'",
 			"'+lastname.to_s+'",
@@ -44,15 +44,16 @@ def add_contact()
 			"'+phone.to_s+'"
 		);'
 end	
-
+#ask the user what he/she would like to do and save the choice in a variable
 puts 'What would you like to do?'
 puts 'show contacts'
 puts 'add contact'
-puts 'remove contact'
+#puts 'remove contact'
+puts
 choice = gets.chomp
-
-if choice == 'show contacts'
+#
+if choice == 'show contacts' || choice == 'show'
 	show_contacts
-elsif choice == 'add contact'
+elsif choice == 'add contact' || choice == 'add'
 	add_contact
 end
